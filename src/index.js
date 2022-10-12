bb41d5c9467addbbfb69407a8e02b448cbbf2d75
felipe bastos <felipebastosweb@gmail.com>
2022-10-12 00:53:30 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import { FirstLevel } from "./levels/first_level"
 import { ActorDocPage } from "./docs/actor"
 import { UserStoryDocPage } from "./docs/user_story"
 
-/*
-import { AboutPage } from "./pages/About"
-import { ProjectsPage } from "./pages/Projects"
-import { PartnershipPage } from "./pages/Partnership"
-import { ContactPage } from "./pages/Contact"
-import { ShopPage } from "./pages/Shop"
-import { CartPage } from "./pages/Cart"
-*/
 
 var root = document.body
 
