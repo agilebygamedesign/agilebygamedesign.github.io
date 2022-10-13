24fd1f02169598db3201fb0e01abb90c2af37869
felipe bastos <felipebastosweb@gmail.com>
2022-10-13 17:01:29 -0300

    Add files via upload

--- src/scenes/actor_scene.js
+++ src/scenes/actor_scene.js
@@ -1,5 +1,7 @@
 import m from "mithril"
-import { Scene } from "./scene";
+import { Scene } from "./scene"
+import { FirstLevel } from "../levels/first_level"
+import { HallScene } from "./hall_scene"
 
 export class ActorScene extends Scene {
     
@@ -10,12 +12,32 @@ export class ActorScene extends Scene {
     view(vnode) {
         return m(".container", [
             m(".row", [
+                m("nav.breadcrumb", [
+                    m("o.breadcrumb", [
+                        m("li.breadcrumb-item", [
+                            m("a", {
+                                href: "#",
+                                onclick: function() {
+                                    FirstLevel.scene_active = m(HallScene)
+                                }
+                            }, "Home")
+                        ]),
+                        m("li.breadcrumb-item.active", [
+                            m("a", {
+                                "data-bs-toggle": "tooltip",
+                                "data-bs-title": "You are here."
+                            }, "User or Actor"),
+                        ]),
+                    ]),
+                ]),
+            ]),
+            m(".row", [
                 m(".col-12", [
                     m("h2", "User or Actor"),
                     m("img", {src: "static/images/actors/actors_example.png", width: "90%", height: "90%", style: "display: block; margin: 0 auto;"}),
-                    m("p", "Coming Soon"),
+                    //m("p", "Coming Soon"),
                 ]),
-            ])
+            ]),
         ])
     }
 }
--- src/scenes/user_story_scene.js
+++ src/scenes/user_story_scene.js
@@ -1,5 +1,7 @@
 import m from "mithril"
-import { Scene } from "./scene";
+import { Scene } from "./scene"
+import { FirstLevel } from "../levels/first_level"
+import { HallScene } from "./hall_scene"
 
 export class UserStoryScene extends Scene {
     
@@ -10,9 +12,33 @@ export class UserStoryScene extends Scene {
     view(vnode) {
         return m(".container", [
             m(".row", [
+                m("nav.breadcrumb", [
+                    m("o.breadcrumb", [
+                        m("li.breadcrumb-item", [
+                            m("a", {
+                                href: "#",
+                                onclick: function() {
+                                    FirstLevel.scene_active = m(HallScene)
+                                }
+                            }, "Home")
+                        ]),
+                        m("li.breadcrumb-item.active", [
+                            m("a", {
+                                "data-bs-toggle": "tooltip",
+                                "data-bs-title": "You are here."
+                            }, "User Stories"),
+                        ]),
+                    ]),
+                ]),
+            ]),
+            m(".row", [
                 m(".col-12", [
                     m("h2", "User Story"),
-                    m("p", "Coming Soon"),
+                    m("p", "Em um projeto de desenvolvimento ágil os requisitos são descritos no formato de Histórias dos Usuários."),
+                    m("p", "Ela é uma explicação informal e geral sobre um recurso de software escrita a partir da perspectiva do usuário final ou cliente."),
+                    m("p", "A História de Usuário é representada por cartões (Cards), como no exemplo a seguir:"),
+                    m("img", {src: "static/images/userstories/storycards.webp", width: "400px", height: "250px", style: "display: block; margin: 0 auto;"}),
+                    m("p.display-5", "Como um \"Usuário\", eu quero [executar uma ação] para obter [certo benefício]."),
                 ]),
             ])
         ])
