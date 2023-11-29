7c7ce8bb2eb4f42ea7d67d1dc774109306f2eaf6
felipe bastos <felipebastosweb@gmail.com>
2023-11-29 09:41:50 -0300

    Update card_scenario.js

--- src/components/card_scenario.js
+++ src/components/card_scenario.js
@@ -14,7 +14,8 @@ export class CardScenario {
                     }
                 }, [
                 m("h5.card-title", "Scenario"),
-                m("p.card-text", "A User Story Scenario (or \"Scene\") is a set of Game World Components in which Actors interact with Game Entities."),
+                m("p.card-text", "A User Story Scenario (or \"Scene\") is a set of Rules and Game World Components in which Actors interact with other Game Entities in order to achieve a specific goal.
+​"),
                 m("p.card-text", "Coming soon"),
             ]),
         ])
